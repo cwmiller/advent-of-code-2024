c32e741684f86ff1fc13c012af2827e6cb62180e
Chase Miller <chase@miller.sh>
2024-12-03 12:59:56 -0500

    Small naming tweaks

--- day3/day3.go
+++ day3/day3.go
@@ -17,11 +17,11 @@ func main() {
 
 	contents := readInputfile(os.Args[1])
 
-	// Result will be the sum of the results of all mul() instructions in the input
+	// The sum of the results of all mul() instructions in the input
 	results := 0
 
 	// For part 2, only the results of mul() instructions following do() instructions count
-	conditionalResult := 0
+	conditionalResults := 0
 
 	// mul instructions are in the format "mul(X,Y) where and Y are 1-3 digit numbers
 	mulRx, _ := regexp.Compile(`mul\((\d{1,3}),(\d{1,3})\)`)
@@ -30,11 +30,13 @@ func main() {
 	allCommandsRx, _ := regexp.Compile(`do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)`)
 
 	// Find the start & end indexes of all commands within the input
-	matches := allCommandsRx.FindAllStringIndex(string(contents), -1)
+	commands := allCommandsRx.FindAllStringIndex(string(contents), -1)
 
+	// As we iterate through all commands found, mulEnabled will be toggled as it finds do() and don't() commands
+	// mul() commands only apply in Part 2 if a do() command proceeded it
 	mulEnabled := true
 
-	for _, indexes := range matches {
+	for _, indexes := range commands {
 		startIdx, endIdx := indexes[0], indexes[1]
 		command := contents[startIdx:endIdx]
 
@@ -55,13 +57,13 @@ func main() {
 			results += result
 
 			if mulEnabled {
-				conditionalResult += result
+				conditionalResults += result
 			}
 		}
 	}
 
 	fmt.Println("Part 1 result:", results)
-	fmt.Println("Part 2 result:", conditionalResult)
+	fmt.Println("Part 2 result:", conditionalResults)
 }
 
 func readInputfile(filename string) string {
